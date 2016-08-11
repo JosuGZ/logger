2f6e05cc22da5cdf46a94a2b8535e68e56f2a584
Josu Goñi <josu_z@hotmail.com>
2016-08-11 21:50:00 +0200

    Código base

---
+++ mainwindow.cpp
@@ -0,0 +1,61 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <stdlib.h>
+#include <time.h>
+
+#include "QDateTime"
+#include <QClipboard>
+#include <QClipboard>
+
+#include <string>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    QTextDocument *log = new QTextDocument("log");
+    ui->logViewer->setDocument(log);
+    connect(ui->Enter,SIGNAL(clicked()),this,SLOT(paste()));
+    connect(ui->actionToggleOnTop,SIGNAL(triggered()),this,SLOT(toggleOnTop()));
+    connect(ui->actionUndo,SIGNAL(triggered()),this,SLOT(undo()));
+    connect(ui->enterButton,SIGNAL(clicked()),this,SLOT(enter()));
+    connect(ui->lineEdit,SIGNAL(returnPressed()),this,SLOT(enter()));
+}
+
+void MainWindow::toggleOnTop() {
+    if( this->windowFlags() & Qt::WindowStaysOnTopHint ) {
+        this->setWindowFlags(this->windowFlags() & ~Qt::WindowStaysOnTopHint);
+    } else {
+        this->setWindowFlags(this->windowFlags() | Qt::WindowStaysOnTopHint);
+    }
+    this->show();
+}
+
+void MainWindow::paste() {
+    //QDate now = QDate::currentDate();
+    const QClipboard *clipboard = QApplication::clipboard();
+    ui->logViewer->appendPlainText(
+                QDateTime::currentDateTime().toString("hh:mm:ss") +
+                " -> " +
+                clipboard->text());
+}
+
+void MainWindow::enter() {
+    ui->logViewer->appendPlainText(
+                QDateTime::currentDateTime().toString("hh:mm:ss") +
+                " -> " +
+                ui->lineEdit->text());
+    ui->lineEdit->clear();
+}
+
+void MainWindow::undo() {
+    ui->logViewer->document()->undo();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
