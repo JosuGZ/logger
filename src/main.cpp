a0cc466a4a00029ae31ca85733dd73fc25d126dd
Josu Goñi <josu_z@hotmail.com>
2020-04-06 10:57:06 +0200

    Replace __windows__ with __WIN32__

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,19 @@
 #include "mainwindow.h"
 
 
-#ifdef __windows__
+#ifdef __WIN32__
 #include "eventfilter.h"
 #endif
 #include <QApplication>
 
-#ifdef __windows__
+#ifdef __WIN32__
 void registerSessionNotification(WId winId);
 #endif
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
-#ifdef __windows__
+#ifdef __WIN32__
     registerSessionNotification(w.winId());
     EventFilter *eventFilter = new EventFilter(w);
     a.installNativeEventFilter(eventFilter);
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]) {
     return a.exec();
 }
 
-#ifdef __windows__
+#ifdef __WIN32__
 void registerSessionNotification(WId winId) {
     // https://docs.microsoft.com/es-es/windows/desktop/api/wtsapi32/nf-wtsapi32-wtsregistersessionnotification
     WTSRegisterSessionNotification(
